47ee5083a47ffaee498faca8a8f9289da81403e9
Zakjer <340631@uwr.edu.pl>
2024-03-26 12:13:42 +0100

    Working on filtering system

--- CarFinder.py
+++ CarFinder.py
@@ -2,6 +2,7 @@
 from bs4 import BeautifulSoup
 from random import randint 
 from time import sleep 
+from statistics import fmean
 
 def get_information(informations):
         """Function for extracting information from html page"""
@@ -18,11 +19,32 @@ def extract_year_and_name(title):
     year = parts[0]
     name = parts[1]
     return year, name
+
+def average_car_price(price_string_list):
+    """Function for calculating the average car price"""
+    price_float_list = []
+
+    for price in price_string_list:
+        price_only_numbers = price.replace('$', '').replace(',', '')
+        price_float = float(price_only_numbers)
+        price_float_list.append(price_float)
+
+    return fmean(price_float_list)
     
 cars_info = []
 page_number = 1
 
 car_model = input('Please type the name of the car model that you want to search for: ')
+
+specify_data = input('Do you want to specify year of production and mileage? ("yes" or "no"): ')
+if specify_data.lower() == "yes":
+    min_year = int(input('Please enter the minimum year: '))
+    max_year = int(input('Please enter the maximum year: '))
+    min_mileage = int(input('Please enter the minimum mileage: '))
+    max_mileage = int(input('Please enter the maximum mileage: '))
+else:
+    pass
+
 print('The results will be ready in a few seconds. Please wait...')
 
 while page_number < 5:
@@ -52,3 +74,4 @@ while page_number < 5:
     sleep(randint(2,5))
 
 print('\n', cars_info)
+print(f'The average car price is: {average_car_price(price)}')
